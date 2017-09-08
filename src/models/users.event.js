5a2a293ce979446c75ed0a997ca7db73204ea2ea
valleykid <valleykiddy@gmail.com>
2017-09-08 14:22:27 +0800

    Event-callback arguments order update

--- src/models/users.event.js
+++ src/models/users.event.js
@@ -1,29 +1,29 @@
 import { routerRedux } from 'dva/router';
 
-export function deleteHandler(targetProps) {
-  const { record: { id } } = targetProps;
+export function deleteHandler(e, { props }) {
+  const { record: { id } } = props;
   this.props.dispatch({
     type: 'users/remove',
     payload: id,
   });
 }
 
-export function pageChangeHandler(targetProps, page) {
+export function pageChangeHandler(page) {
   this.props.dispatch(routerRedux.push({
     pathname: '/users',
     query: { page },
   }));
 }
 
-export function editHandler(targetProps, values) {
-  const { record: { id } } = targetProps;
+export function editHandler(values, { props }) {
+  const { record: { id } } = props;
   this.props.dispatch({
     type: 'users/patch',
     payload: { id, values },
   });
 }
 
-export function createHandler(targetProps, values) {
+export function createHandler(values) {
   this.props.dispatch({
     type: 'users/create',
     payload: values,
